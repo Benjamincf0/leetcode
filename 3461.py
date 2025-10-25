2a3fbfa6267faa04163e2a945c41d9565cd2f0bd
Benjamin <benjamincf01@gmail.com>
2025-10-24 20:08:45 -0400

    daily #3461 speed improved

--- 3461.py
+++ 3461.py
@@ -1,12 +1,11 @@
 # easy
 # 3461. Check If Digits Are Equal in String After Operations I
+# this is faster because list comprehension runs in C and ord() is faster than int().
+# convert string to list, then run loops
 class Solution:
     def hasSameDigits(self, s: str) -> bool:
-        while len(s) > 2:
-            s_new = ""
-            for i in range(len(s) - 1):
-                res = (int(s[i]) + int(s[i+1])) % 10
-                s_new += str(res)
-            
-            s = s_new
-        return s[0] == s[1]
+        s_list = [ord(c) - ord('0') for c in s]
+        n = len(s)
+        while len(s_list) > 2:
+            s_list = [((s_list[i] + s_list[i+1]) % 10) for i in range(len(s_list) - 1)]
+        return s_list[0] == s_list[1]
