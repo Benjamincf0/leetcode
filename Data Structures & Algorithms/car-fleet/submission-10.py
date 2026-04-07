e2070fd5267ae7ab55600ac0f3abe4e129059fa3
Benjamin <83923053+Benjamincf0@users.noreply.github.com>
2026-04-07 15:46:47 -0400

    Add: car-fleet - submission-10

---
+++ Data Structures & Algorithms/car-fleet/submission-10.py
@@ -0,0 +1,16 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = sorted(list(zip(position, speed)), key=lambda x: x[0], reverse=True)
+
+        numFleets = 0
+        nextTimeToTarget = 0 # pos is strictly less than target
+        for pos, speed in pairs:
+            timeToTarget = (target-pos)/speed
+
+            # current is slower than next (or empty stack)
+            # => implies they must NOT intersect => new fleet
+            if timeToTarget > nextTimeToTarget:
+                nextTimeToTarget = timeToTarget
+                numFleets += 1
+
+        return numFleets
