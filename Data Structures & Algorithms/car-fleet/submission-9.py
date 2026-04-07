97641693fc97eae7aace4bd368968359e0a4fa4b
Benjamin <83923053+Benjamincf0@users.noreply.github.com>
2026-04-07 15:27:22 -0400

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pairs = list(zip(position, speed))
+        pairs.sort(key=lambda x: x[0])
+
+        stack = []
+        for pos, speed in pairs[::-1]:
+            timeToTarget = (target-pos)/speed
+
+            if not stack or timeToTarget > stack[-1]: stack.append(timeToTarget) # current is slower than next (or empty stack)
+
+        return len(stack)
