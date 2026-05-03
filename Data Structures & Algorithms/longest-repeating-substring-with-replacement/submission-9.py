83321dbcddd766629ecab45106821f85cf1aada3
Benjamin <83923053+Benjamincf0@users.noreply.github.com>
2026-05-03 00:29:24 -0400

    Add: longest-repeating-substring-with-replacement - submission-9

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # Time O(n) Space O(n)
+        l=0
+        d = defaultdict(int) # frequency of each letter in current window.
+        res = 0 # len of max string with k permutations.
+
+        for r in range(len(s)):
+            d[s[r]]+=1
+            len_window = r-l+1
+            # a window is valid if we can make at most k mutations 
+            # and obtain a 1 char string
+            valid_window = len_window - max(d.values()) <= k
+            if not valid_window:
+                d[s[l]]-=1
+                l+=1
+            
+            res = max(res, r-l+1)
+
+        return res
