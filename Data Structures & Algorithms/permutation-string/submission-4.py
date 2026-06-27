475a7915b789b7ede2855e404f0b0d2b7c215fb5
Benjamin <83923053+Benjamincf0@users.noreply.github.com>
2026-06-27 01:24:47 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_map = {}
+        for letter in s1:
+            if letter in s1_map:
+                s1_map[letter] += 1
+            else:
+                s1_map[letter] = 1
+
+        print(s1_map)
+
+        l = 0
+        while l <= len(s2)-len(s1):
+            r = l+len(s1)
+            hm = defaultdict(int)
+            for i in range(l, r):
+                letter = s2[i]
+                hm[letter]+=1
+                if letter not in s1_map:
+                    l+=i-l
+                    break
+            l+=1
+            print(hm)
+            if hm == s1_map: return True
+            print(l, len(s2)-len(s1))  
+            print(l < len(s2)-len(s1))
+        
+        return False
