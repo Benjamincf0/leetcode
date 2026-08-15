48b32c76468eae3bbea36e1e6e6873041e1bc924
Benjamin <83923053+Benjamincf0@users.noreply.github.com>
2026-08-15 19:57:40 -0400

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # Time O(2^t/m) ; Space O(t/m) ; where t=target and m=min(nums)
+        res = []
+
+        current = []
+
+        def dfs(idx: int, running_total: int):
+            if running_total == target:
+                res.append(current.copy())
+                return
+            elif idx >= len(nums) or running_total > target: return
+
+            # Skip this number
+            dfs(idx+1, running_total)
+
+            # Choose this number ... and maybe we'll choose it again later.
+            current.append(nums[idx])
+            dfs(idx, running_total+nums[idx])
+
+            # backtrack
+            current.pop()
+        
+        dfs(0, 0)
+
+        return res
