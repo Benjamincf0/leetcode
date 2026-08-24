8c6ee91e3830c943a6a2012eb50566f84b05f178
Benjamin <83923053+Benjamincf0@users.noreply.github.com>
2026-08-24 02:36:06 -0400

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,45 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        # Time O(n*n!) ; Space O(n)
+
+        res = []
+        queens = []
+
+        other_cols = set()
+        other_pos_diag = set()
+        other_neg_diag = set()
+        def dfs(c):
+            r = len(queens)
+            # check if in check O(1)
+            same_col = c in other_cols
+            same_pos_diag = (r-c) in other_pos_diag
+            same_neg_diag = (r+c) in other_neg_diag
+            if same_col or same_pos_diag or same_neg_diag: return
+
+            queens.append(c)
+            other_pos_diag.add(r-c)
+            other_neg_diag.add(r+c)
+            other_cols.add(c)
+
+            new_r = r+1
+            if new_r < n:
+                for new_c in range(0, n):
+                    if new_c != c:
+                        dfs(new_c)
+            else:
+                # save sln
+                sln = []
+                for col in queens:
+                    s = "."*col+"Q"+"."*(n-col-1)
+                    sln.append(s)
+                res.append(sln)
+
+            queens.pop()
+            other_pos_diag.remove(r-c)
+            other_neg_diag.remove(r+c)
+            other_cols.remove(c)
+
+        for c in range(n):
+            dfs(c)
+
+        return res
