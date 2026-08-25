642a323c9577aad1fdc31cf97bd069823ef794df
Benjamin <83923053+Benjamincf0@users.noreply.github.com>
2026-08-25 15:47:40 -0400

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # Time O(n) ; Space 
+
+        d = defaultdict(list)
+
+        for a, b in edges:
+            d[a].append(b)
+            d[b].append(a)
+
+        root = 0
+
+        visited = set()
+        q = deque()
+        q.append((root, -1))
+
+        while q:
+            node, prev_node = q.popleft()
+
+            # if there's a cycle, we'll end up with the same node added twice to the queue.
+            if node in visited:
+                return False
+            
+            visited.add(node)
+
+            children = d[node]
+            for c in children:
+                if c != prev_node:
+                    q.append((c, node))
+
+        is_connected_graph = len(visited) == n
+        return is_connected_graph
