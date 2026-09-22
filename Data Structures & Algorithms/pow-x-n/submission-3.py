d416cbf6a7bff08979df54ff85164c8fe24e1115
Benjamin <83923053+Benjamincf0@users.noreply.github.com>
2026-09-21 22:24:15 -0400

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # Time O(log(n)) Space O(log(n))
+        if n < 0:
+            x = 1 / x
+            n = -n
+        if n == 0: return 1
+        elif n == 1: return x
+
+        tot = 1
+        while n > 0:
+            if n == 1: return res*x
+            i = 1
+            res = x
+            while 2*i <= n:
+                res *= res
+                i *= 2
+            n -= i
+            tot *= res
+
+        return tot
